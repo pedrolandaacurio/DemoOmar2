acc14c29f99511ac95fd574213a566552f2ec1b5
pedrolandaacurio <pedrolandaacurio@hotmail.com>
2017-10-06 11:32:55 -0500

    Agregar archivos de proyecto.

---
+++ DemoOmar2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoOmar2.Startup))]
+namespace DemoOmar2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
